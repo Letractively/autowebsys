3dcf5991ae6cb120de30278a8f920d4a4e955d82
kopacki.tomasz <kopacki.tomasz@gmail.com>
2011-02-14 17:42:13 +0000

    W nawiązaniu do: http://code.google.com/p/autowebsys/issues/detail?id=4
    Ikonka od zwijania okienka nie chowa go tylko zwija do postaci samej belki.
    Ikonka powiększania okna nie odświeża go, przez co zawartość nie powiększa się do nowego rozmiaru.
    
    Błędy poprawnione

---  autowebsys/autowebsys/public/application.js
+++  autowebsys/autowebsys/public/application.js
@@ -1,149 +1,249 @@
-function Application() {
-}
-
-Application.prototype.initDom = function() {
-    this.desktop = document.createElement('div');
-    this.desktop.id = 'desktop';
-    this.desktop.style.border = '1px solid gray';
-    this.desktop.style.height = (this.getFullHeight() - 45) + 'px';
-    this.taskbar = document.createElement('div');
-    this.taskbar.id = 'taskbar';
-    document.body.appendChild(this.desktop);
-    document.body.appendChild(this.taskbar);
-    this.windowsManager = new WindowsManager(this.desktop);
-    this.mainMenu = new MainMenu(this.taskbar);
-}
-
-Application.prototype.initEvents = function() {
-    if(window.addEventListener) {
-        window.addEventListener('resize', this.onResize, false);
-    } else if(window.attachEvent) {
-        window.attachEvent('onresize', this.onResize);
-    } else {
-        window.onresize = this.onResize;
+utils = {
+    uid: 0,
+    getFullHeight: function() {
+        if(typeof(window.innerHeight) == 'number' ) {
+            return window.innerHeight;
+        } else if(document.documentElement && (document.documentElement.clientWidth || document.documentElement.clientHeight)) {
+            return document.documentElement.clientHeight;
+        } else if(document.body && (document.body.clientWidth || document.body.clientHeight)) {
+            return document.body.clientHeight;
+        }
+        return 0;
+    },
+    initGlobalEvents: function() {
+        if(window.addEventListener) {
+            window.addEventListener('resize', utils.onResize, false);
+        } else if(window.attachEvent) {
+            window.attachEvent('onresize', utils.onResize);
+        } else {
+            window.onresize = utils.onResize;
+        }
+    },
+    onResize: function() {
+        application.controlls.desktop.setHeight((utils.getFullHeight() - 45) + 'px');
+    },
+    generateUID: function() {
+        this.uid += 1;
+        return this.uid;
     }
 }
 
-Application.prototype.getFullHeight = function() {
-    if(typeof(window.innerHeight) == 'number' ) {
-        return window.innerHeight;
-    } else if(document.documentElement && (document.documentElement.clientWidth || document.documentElement.clientHeight)) {
-        return document.documentElement.clientHeight;
-    } else if(document.body && (document.body.clientWidth || document.body.clientHeight)) {
-        return document.body.clientHeight;
+validators = {
+    minLength: function(string, length) {
+        return (string.length < length) ? false : true;
+    },
+
+    maxLength: function(string, length) {
+        return (string.length > length) ? false : true;
+    },
+
+    isNumber: function(string) {
+        return string.isNumber();
     }
-    return 0;
-}
-
-Application.prototype.onResize = function(x, y) {
-    application.desktop.style.height = (application.getFullHeight() - 45) + 'px';
-}
-
-function WindowsManager(div) {
-    this.windowsManager = new dhtmlXWindows();
-    this.windowsManager.enableAutoViewport(false);
-    this.windowsManager.attachViewportTo(div.id);
-    this.windowsManager.setImagePath('/dhtmlx/imgs/');
-    this.uid = 1;
-    this.windows = Array();
-}
-
-WindowsManager.prototype.createWindow = function(title, pos_x, pos_y, width, height, icon, content) {
-    this.uid++;
-    icon = icon || 'new.gif';
-    content = content || 'not found';
-    this.windows[this.uid] = this.windowsManager.createWindow(this.uid);
-    this.windows[this.uid].setDimension(width, height);
-    this.windows[this.uid].setPosition(pos_x, pos_y);
-    this.windows[this.uid].uid = this.uid;
-    this.windows[this.uid].setText(title);
-    this.windows[this.uid].attachHTMLString(content);
-    this.windows[this.uid].attachEvent('onClose', application.windowsManager.onClose);
-    application.mainMenu.addWindowToTask(this.uid, title, icon);
-}
-
-WindowsManager.prototype.onClose = function(window) {
-    window.hide();
-    application.windowsManager.windows[window.uid] = null;
-    application.mainMenu.removeWindowFromTask(window.uid);
-}
-
-function MainMenu(div) {
-    this.start = document.createElement('div');
-    this.start.id = 'start';
-    this.start.style.width = '60px';
-    this.start.style.cssFloat = 'left';
-    this.bar = document.createElement('div');
-    this.bar.id = 'bar';
-    div.appendChild(this.start);
-    div.appendChild(this.bar);
-    this.menu = new dhtmlXMenuObject(this.start.id, 'dhx_skyblue');
-    this.menu.setIconsPath("/imgs/");
-    this.menu.loadXML('/data/index/type/main-menu');
-    this.menu.attachEvent("onClick", this.clickOnMenu);
-    this.taskbar = new dhtmlXToolbarObject(this.bar.id, 'dhx_skyblue');
-    this.taskbar.setIconPath("/imgs/");
-    this.taskbar.attachEvent('onClick', this.clickOnTaskbar);
-}
-
-MainMenu.prototype.clickOnMenu = function(id) {
-    if(id.startsWith("m_")) {
-        application.windowsManager.getWindow(id.substring(2, id.length));
-    } else {
-//logout
-}
-}
-
-MainMenu.prototype.addWindowToTask = function(id, title, icon) {
-    this.taskbar.addButton('t' + id, null, title, icon);
-}
-
-MainMenu.prototype.removeWindowFromTask = function(id) {
-    this.taskbar.removeItem('t' + id);
-}
-
-MainMenu.prototype.clickOnTaskbar = function(id) {
-    id = id.substring(1, id.length);
-    if(application.windowsManager.windows[id].isHidden() || !application.windowsManager.windows[id].isOnTop()) {
-        application.windowsManager.windows[id].show();
-        application.windowsManager.windows[id].bringToTop();
-    } else if(application.windowsManager.windows[id].isOnTop()) {
-        application.windowsManager.windows[id].hide();
-    }
-}
-
-WindowsManager.prototype.getWindow = function(id) {
-    var serializer = new XMLSerializer();
-    var description = dhtmlxAjax.getSync("/data/index/type/window-content/name/" + id).xmlDoc.responseXML;
-    var title = description.getElementsByTagName('title')[0];
-    title = (title ? title.textContent : "");
-    var pos_x = description.getElementsByTagName('pos_x')[0];
-    pos_x = (pos_x ? pos_x.textContent : undefined);
-    var pos_y = description.getElementsByTagName('pos_y')[0];
-    pos_y = (pos_y ? pos_y.textContent : undefined);
-    var width = description.getElementsByTagName('width')[0];
-    width = (width ? width.textContent : undefined);
-    var height = description.getElementsByTagName('height')[0];
-    height = (height ? height.textContent : undefined);
-    var icon = description.getElementsByTagName('icon')[0];
-    icon = (icon ? icon.textContent : undefined);
-    var content = description.getElementsByTagName('content')[0];
-    if(content) {
-        content = serializer.serializeToString(content);
-        alert(content);
-        var begin = 9;
-        var length = content.length - 10;
-        alert(begin + " " + length);
-        content = content.substring(begin, length);
-        alert(content);
-    }
-    this.createWindow(title, pos_x, pos_y, width, height, icon, content);
 }
 
 String.prototype.startsWith = function(str){
     return (this.indexOf(str) === 0);
 }
 
-var application = new Application();
-application.initDom();
-application.initEvents();
+function Desktop() {
+    this.desktop = document.createElement('div');
+    this.desktop.id = 'desktop';
+    this.desktop.style.border = '1px solid gray';
+    this.desktop.style.height = (utils.getFullHeight() - 45) + 'px';
+    document.body.appendChild(this.desktop);
+    this.windowsManager = new dhtmlXWindows();
+    this.windowsManager.enableAutoViewport(false);
+    this.windowsManager.attachViewportTo(this.desktop.id);
+    this.windowsManager.setImagePath('/dhtmlx/imgs/');
+    this.windows = Array();
+}
+
+Desktop.prototype.setHeight = function(height) {
+    this.desktop.style.height = height;
+}
+
+Desktop.prototype.createWindow = function(window) {
+    var uid = utils.generateUID();
+    this.windows[uid] = this.windowsManager.createWindow(uid, window.get('posX'), window.get('posY'), window.get('width'), window.get('height'));
+    this.windows[uid].uid = this.uid;
+    this.windows[uid].setText(window.get('title'));
+    this.windows[uid].windowURL = window.get('content');
+    this.windows[uid].attachURL(this.windows[uid].windowURL, true);
+    this.windows[uid].attachEvent('onClose', this.onClose);
+    this.windows[uid].attachEvent('onResizeFinish', this.onResizeFinish);
+    this.windows[uid].attachEvent('onMinimize', this.onResizeFinish);
+    this.windows[uid].attachEvent('onMaximize', this.onResizeFinish);
+    this.windows[uid].attachEvent('onParkUp', this.onParkUp);
+    application.controlls.bar.taskbar.addWindow(uid, window.get('title'), window.get('icon'));
+}
+
+Desktop.prototype.toolbarAction = function(id) {
+    var window;
+    var selectedId;
+    switch(id) {
+        case "add":
+            window = application.communicator.getWindow(this.addWindow);
+            application.controlls.desktop.createWindow(window);
+            break;
+        case "edit":
+            selectedId = this.grid.getSelectedRowId();
+            if(selectedId != null) {
+                window = application.communicator.getWindow(this.editWindow + "/id/" + selectedId);
+                application.controlls.desktop.createWindow(window);
+            } else {
+                alert(this.grid.notSelectedWarn);
+            }
+            break;
+        case "delete":
+            selectedId = this.grid.getSelectedRowId();
+            if(selectedId != null) {
+                alert(this.grid.getSelectedRowId());
+            } else {
+                alert(this.grid.notSelectedWarn);
+            }
+            break;
+        case "refresh":
+            this.grid.updateFromXML(this.grid.url);
+            break;
+        case "save":
+            alert(this.grid);
+            break;
+    }
+}
+
+Desktop.prototype.onParkUp = function(event) {
+    var window = application.controlls.desktop.windows[event.idd];
+    window.hide();
+}
+
+Desktop.prototype.onResizeFinish = function(event) {
+    var window = application.controlls.desktop.windows[event.idd];
+    window.attachURL(window.windowURL, true);
+}
+
+Desktop.prototype.onClose = function(window) {
+    window.hide();
+    application.controlls.bar.taskbar.removeWindow(window.idd);
+    application.controlls.desktop.windows[window.idd] = null;
+}
+
+function MainMenu(holder) {
+    this.start = document.createElement('div');
+    this.start.id = 'start';
+    this.start.style.width = '70px';
+    this.start.style.cssFloat = 'left';
+    holder.appendChild(this.start);
+    this.menu = new dhtmlXMenuObject(this.start.id, 'dhx_skyblue');
+    this.menu.setIconsPath("/imgs/");
+    this.menu.loadXML('/data/index/type/main-menu');
+    this.menu.attachEvent("onClick", this.clickOnMenu);
+}
+
+MainMenu.prototype.clickOnMenu = function(id) {
+    if(id.startsWith("m_")) {
+        var window = application.communicator.getWindow(id.substring(2, id.length));
+        application.controlls.desktop.createWindow(window);
+    } else {
+        alert('logout');//logout
+    }
+}
+
+function Taskbar(holder) {
+    this.bar = document.createElement('div');
+    this.bar.id = 'taskbar';
+    holder.appendChild(this.bar);
+    this.taskbar = new dhtmlXToolbarObject(this.bar.id, 'dhx_skyblue');
+    this.taskbar.setIconPath("/imgs/");
+    this.taskbar.attachEvent('onClick', this.onClick);
+}
+
+Taskbar.prototype.onClick = function(id) {
+    id = id.substring(1, id.length);
+    var windows = application.controlls.desktop.windows;
+    if(windows[id].isHidden()) {
+        windows[id].show();
+        windows[id].park();
+    } else if(windows[id].isOnTop()) {
+        windows[id].hide();
+    } else {
+        windows[id].bringToTop();
+    }
+}
+
+Taskbar.prototype.addWindow = function(id, title, icon) {
+    this.taskbar.addButton('t' + id, null, title, icon);
+}
+
+Taskbar.prototype.removeWindow = function(id) {
+    this.taskbar.removeItem('t' + id);
+}
+
+function Bar() {
+    this.taskbar = document.createElement('div');
+    this.taskbar.id = 'bar';
+    document.body.appendChild(this.taskbar);
+    this.mainMenu = new MainMenu(this.taskbar);
+    this.taskbar = new Taskbar(this.taskbar);
+}
+
+function Controlls() {
+    this.desktop = new Desktop();
+    this.bar = new Bar();
+}
+
+function Communicator() {
+
+}
+
+Communicator.prototype.getWindow = function(id) {
+    var url = "/data/index/type/window-description/name/" + id;
+    if(arguments.length > 1) {
+        url += arguments[1];
+    }
+    var xml = dhtmlxAjax.getSync(url).xmlDoc.responseXML;
+    xml = (xml.childNodes[0].childNodes.length == 0 ? xml.childNodes[1] : xml.childNodes[0]); //IE
+    var window = new Window();
+    for(var i = 0 ; i < xml.childNodes.length ; i++) {
+        var item = xml.childNodes.item(i);
+        if(item.nodeType == 1) {
+            var name = item.nodeName;
+            var value;
+            try {
+                value = (new XMLSerializer()).serializeToString(item);
+            } catch(e) {
+                value = item.xml; //IE
+            }
+            value = value.substring(name.length + 2, value.length - name.length - 3);
+            window.set(name, value);
+        }
+    }
+    window.set('content', '/data/index/type/window-content/name/' + id);
+    return window;
+}
+
+function Application() {
+    this.controlls = new Controlls();
+    this.communicator = new Communicator();
+    utils.initGlobalEvents();
+}
+
+function Window() {
+    this.items = Array();
+    this.items['title'] = 'window title';
+    this.items['posX'] = 10;
+    this.items['posY'] = 10;
+    this.items['width'] = 500;
+    this.items['height'] = 200;
+    this.items['content'] = 'empty';
+    this.items['icon'] = 'new.gif';
+}
+
+Window.prototype.set = function(name, value) {
+    this.items[name] = value;
+}
+
+Window.prototype.get = function(name) {
+    return this.items[name];
+}
+
