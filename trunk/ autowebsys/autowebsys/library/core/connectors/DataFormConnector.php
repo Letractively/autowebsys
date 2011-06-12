0009bc047928d56438c99c5b68e068ee22c2b5a9
kopacki.tomasz@gmail.com <kopacki.tomasz@gmail.com@6806d879-7a3f-edc9-839f-aecd0b24e53d>
2011-06-12 10:43:33 +0000

    Wyłączyłem blokade generowanie pustych atrybutow w formularzach. Nie jestem pewien po co ja wcześneij ustawiłem...

--- trunk/ autowebsys/autowebsys/library/core/connectors/DataFormConnector.php
+++ trunk/ autowebsys/autowebsys/library/core/connectors/DataFormConnector.php
@@ -31,9 +31,7 @@ abstract class DataFormConnector extends DataConnector {
         $out .= "<data>";
         if (count($data) == 1) {
             foreach ($data[0] as $key => $value) {
-                if($value != "") {
-                    $out .= "<$key>$value</$key>";
-                }
+                $out .= "<$key>$value</$key>";
             }
         }
         $out .= "</data>";
